b89ca3bc5f62d691f85c1cd28ec25e40a7b989d4
HsiangNianian <i@jyunko.cn>
2026-05-07 10:29:23 +0800

    chore: change questions per test to 20

--- src/lib/questions.ts
+++ src/lib/questions.ts
@@ -10,4 +10,4 @@ export interface Question {
 
 export { selectQuestions } from "./question-bank"
 export const QUESTION_TIME = 40 // seconds per question
-export const QUESTIONS_PER_TEST = 5
+export const QUESTIONS_PER_TEST = 20
